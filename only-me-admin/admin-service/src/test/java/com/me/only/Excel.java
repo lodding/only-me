a1e2cca3f211ab7ecf4e842f3f6258f40d982881
szh <Guest@2022>
2022-05-31 10:34:56 +0800

    update .gitignore

---
+++ only-me-admin/admin-service/src/test/java/com/me/only/Excel.java
@@ -0,0 +1,13 @@
+package com.me.only;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Excel {
+
+    @Test
+    public void  test01(){
+
+    }
+}
